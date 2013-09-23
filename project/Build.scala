9fedaf566ecff5d93c559a9487be85e404e857cf
ygrabovskiy <ygrabovskiy@naumen.ru>
2013-09-23 18:27:55 +0600

    play 2.1.5

--- project/Build.scala
+++ project/Build.scala
@@ -3,25 +3,20 @@ import Keys._
 
 object Build extends Build {
 
-  lazy val playVersion =  "2.1.4"
+    lazy val playVersion = "2.1.5"
 
-  lazy val play =  "play" %% "play" % playVersion
-//
-  lazy val root = Project(id = "play-forms-improved", base = file(".")).settings(
-    version := playVersion + "_0.0.1-SNAPSHOT",
+    lazy val play = "play" %% "play" % playVersion % "provided"
+    //
+    lazy val root = Project(id = "play-forms-improved", base = file(".")).settings(
+        version := playVersion + "_0.0.1-SNAPSHOT",
 
-  libraryDependencies += "com.naumen" %% "forms-dsl" % "0.0.1-SNAPSHOT" ,
+        libraryDependencies += "com.naumen" %% "forms-dsl" % "0.0.1-SNAPSHOT",
 
-  libraryDependencies += "org.specs2" %% "specs2" % "2.1.1" % "test",
+        resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+            "releases" at "http://oss.sonatype.org/content/repositories/releases"),
 
-  scalacOptions in Test ++= Seq("-Yrangepos")           ,
-
-
-  resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-    "releases"  at "http://oss.sonatype.org/content/repositories/releases")   ,
-
-    organization := "com.naumen",
-    libraryDependencies += play,
-    mainClass in (Compile, run) := Some("play.core.server.NettyServer")
-  )
+        organization := "com.naumen",
+        libraryDependencies += play,
+        mainClass in(Compile, run) := Some("play.core.server.NettyServer")
+    )
 }
